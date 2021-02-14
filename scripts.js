305a61dd7b7c07413e11c17cca41fdb46c44afc0
brockhend <78023999+brockhend@users.noreply.github.com>
2021-02-14 11:16:00 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	// Testimonials - Carousel
+	$('.main_hero_content').owlCarousel({
+		items: 1,
+		loop: true,
+		nav: false,
+		dots: false,
+		autoplay: true
+	});
+
+});
